bce11050c7b99c3d09a3409493fb9223b24de9dc
metiftikci <metiftikci@hotmail.com>
2022-06-23 10:07:17 +0300

    Update HOST

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { CardName } from "./models/bluff";
 
-export const HOST = "ws://localhost:3001";
+export const HOST = "wss://node-js-board-games-api.herokuapp.com";
 export const ClubCardNames: CardName[] = [
   "Club A",
   "Club 2",
